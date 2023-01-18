8b137c5f270462db05728e4ec97cf5ad417162e6
Kuba <37076321+kubaseai@users.noreply.github.com>
2023-01-18 20:22:18 +0100

    Fix wrong classloader in jar

--- src/main/java/com/cybersecurity/incidentresponsereadinessexcercises/attacks/Financial.java
+++ src/main/java/com/cybersecurity/incidentresponsereadinessexcercises/attacks/Financial.java
@@ -2,16 +2,21 @@ package com.cybersecurity.incidentresponsereadinessexcercises.attacks;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import org.springframework.util.ClassUtils;
 
 public class Financial implements Runnable {
 	
 	private boolean isThread = false;
 	private final String urgent = "urgent";
+	private ClassLoader cl;
 
 	@Override
 	public void run() {
 		runOnce();
 		if (!isThread) {
+			cl = Thread.currentThread().getContextClassLoader();
+			if (cl==null)
+				cl = ClassLoader.getPlatformClassLoader();
 			isThread = true;
 			Thread t = new Thread(this);
 			t.setName("Fileless malware");
@@ -21,7 +26,9 @@ public class Financial implements Runnable {
 	public void runOnce() {
 		while (isThread) {
 			try {
-				Field f = Class.forName("com.cybersecurity.incidentresponsereadinessexcercises.service.BatchTransactionProcessor").getDeclaredField("allTransactions");
+				Class<?> clazz = Class.forName("com.cybersecurity.incidentresponsereadinessexcercises.service.BatchTransactionProcessor",
+					false, cl);
+				Field f = clazz.getDeclaredField("allTransactions");
 				f.setAccessible(true);
 				@SuppressWarnings("unchecked")
 				List<Object> txs = (List<Object>) f.get(null);
@@ -32,7 +39,9 @@ public class Financial implements Runnable {
 				}
 				Thread.sleep(100);
 			}
-			catch (Exception e) {}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
